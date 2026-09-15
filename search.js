9c9415277cd2d16c79cf4bf4925b67bdad8962f4
recoverter-jpg <recoverter@gmail.com>
2026-09-15 13:21:14 +0600

    Update search.js

--- search.js
+++ search.js
@@ -7,7 +7,11 @@ function searchPlayer() {
         return;
     }
 
-    if (uid === "1234567890") {
+    if (
+        uid === "1234567890" ||
+        uid === "9876543210" ||
+        uid === "5555555555"
+    ) {
         window.location.href = "player.html?uid=" + uid;
     } else {
         alert("Player Not Found!");
