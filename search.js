8dc55ef85a632c307b7da1a1acfcf6ad85f3c162
recoverter-jpg <recoverter@gmail.com>
2026-09-17 00:16:36 +0600

    Update search.js

--- search.js
+++ search.js
@@ -1,19 +1,14 @@
 function searchPlayer() {
 
     const uid = document.getElementById("uidInput").value.trim();
+    const region = document.getElementById("regionSelect").value;
 
     if (uid === "") {
         alert("Please enter a Free Fire UID!");
         return;
     }
 
-    if (
-        uid === "1234567890" ||
-        uid === "9876543210" ||
-        uid === "5555555555"
-    ) {
-        window.location.href = "player.html?uid=" + uid;
-    } else {
-        alert("Player Not Found!");
-    }
+    window.location.href =
+        "player.html?uid=" + encodeURIComponent(uid) +
+        "&region=" + encodeURIComponent(region);
 }
